97484b35ec744afd571737fa54a1929be0e26412
Anastasiya Belousova <104447776+AntraWinter@users.noreply.github.com>
2024-03-24 17:26:54 +0300

    Add files via upload

---
+++ Lab1-OOP/Fraction.cs
@@ -0,0 +1,198 @@
+﻿namespace Lab4_1_OOP_
+{
+    public class Fraction : Object
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public Fraction(int a1, int a2)
+        {
+            if (a2 == 0)
+                throw new ArgumentException("Делить на ноль нельзя", nameof(a2));
+
+            Numerator = a1 / NOD(a1, a2);
+            Denominator = a2 / NOD(a1, a2);
+            if (Numerator < 0)
+            {
+                if (Denominator < 0)
+                {
+                    Numerator = Math.Abs(Numerator);
+                    Denominator = Math.Abs(Denominator);
+                }
+                else
+                {
+                    Numerator = Math.Abs(Numerator);
+                    Denominator = Math.Abs(Denominator);
+                    Numerator = -Numerator;
+                }
+            }
+            else
+            {
+                if (Denominator < 0)
+                {
+                    Numerator = Math.Abs(Numerator);
+                    Denominator = Math.Abs(Denominator);
+                    Numerator = -Numerator;
+                }
+            }
+
+        }
+
+        static int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            if (Numerator == 0) return "0";
+            else
+            {
+                if (Denominator == 1) return Numerator.ToString();
+                else return $"{Numerator}/{Denominator}";
+
+            }
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+                var Numerator = a.Numerator + b.Numerator;
+                var Denominator = a.Denominator;
+                return new Fraction(Numerator, Denominator);
+            }
+            else
+            {
+                var Numerator = (a.Numerator * b.Denominator) + (b.Numerator * a.Denominator);
+                var Denominator = a.Denominator * b.Denominator;
+                return new Fraction(Numerator, Denominator);
+            }
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+                var Numerator = a.Numerator - b.Numerator;
+                var Denominator = a.Denominator;
+                return new Fraction(Numerator, Denominator);
+            }
+            else
+            {
+                var Numerator = (a.Numerator * b.Denominator) - (b.Numerator * a.Denominator);
+                var Denominator = a.Denominator * b.Denominator;
+                return new Fraction(Numerator, Denominator);
+            }
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            
+            var Numerator = a.Numerator * b.Numerator;
+            var Denominator = a.Denominator * b.Denominator;
+            return new Fraction(Numerator, Denominator);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            
+            var Numerator = a.Numerator * b.Denominator;
+            var Denominator = a.Denominator * b.Numerator;
+            return new Fraction(Numerator, Denominator);
+        }
+
+        public static Fraction operator -(Fraction a)
+        {
+            
+            var Numerator = -a.Numerator;
+            var Denominator = a.Denominator;
+            return new Fraction(Numerator, Denominator);
+        }
+
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Numerator == _b.Numerator && _a.Denominator == _b.Denominator) return true;
+            else return false;
+        }
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Numerator != _b.Numerator || _a.Denominator != _b.Denominator) return true;
+            else return false;
+        }
+
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Denominator == _b.Denominator)
+            {
+                if (_a.Numerator < _b.Numerator) return true;
+                else return false;
+            }
+            else
+            {
+                if ((_a.Numerator * _b.Denominator) < (_b.Numerator * _a.Denominator)) return true;
+                else return false;
+            }
+        }
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Denominator == _b.Denominator)
+            {
+                if (_a.Numerator > _b.Numerator) return true;
+                else return false;
+            }
+            else
+            {
+                if ((_a.Numerator * _b.Denominator) > (_b.Numerator * _a.Denominator)) return true;
+                else return false;
+            }
+        }
+
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Denominator == _b.Denominator)
+            {
+                if (_a.Numerator <= _b.Numerator) return true;
+                else return false;
+            }
+            else
+            {
+                if ((_a.Numerator * _b.Denominator) <= (_b.Numerator * _a.Denominator)) return true;
+                else return false;
+            }
+        }
+
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            Fraction _a = a;
+            Fraction _b = b;
+            if (_a.Denominator == _b.Denominator)
+            {
+                if (_a.Numerator >= _b.Numerator) return true;
+                else return false;
+            }
+            else
+            {
+                if ((_a.Numerator * _b.Denominator) >= (_b.Numerator * _a.Denominator)) return true;
+                else return false;
+            }
+        }
+    }
+}
---
+++ Lab1-OOP/Program.cs
@@ -0,0 +1,60 @@
+﻿using Lab4_1_OOP_;
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите первую дробь: ");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        var a = new Fraction(x1, y1);
+        Console.WriteLine(a.ToString());
+        
+        Console.WriteLine("Введите вторую дробь: ");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int y2 = Convert.ToInt32(Console.ReadLine());
+        var b = new Fraction(x2, y2);
+        Console.WriteLine(b.ToString());
+
+        Fraction res = null;
+        Console.WriteLine("\nСумма дробей: ");
+        res = a + b;
+        Console.WriteLine(res.ToString());
+
+
+        Console.WriteLine("\nРазница дробей: ");
+        res = a - b;
+        Console.WriteLine(res.ToString());
+
+        Console.WriteLine("\nПроизведение дробей: ");
+        res = a * b;
+        Console.WriteLine(res.ToString());
+
+
+        Console.WriteLine("\nДеление дробей: ");
+        res = a / b;
+        Console.WriteLine(res.ToString());
+
+
+        Console.WriteLine("\nДробь с противоположным знаком: ");
+        res = -a;
+        Console.WriteLine(res.ToString());
+
+        Console.WriteLine("\nПроверка на == и !=");
+        if (a == b) Console.WriteLine($"{a.ToString()} == {b.ToString()}");
+        else if (a != b) Console.WriteLine($"{a.ToString()} != {b.ToString()}");
+
+
+        Console.WriteLine("\nПроверка на < и >");
+        if (a < b) Console.WriteLine($"{a.ToString()} < {b.ToString()}");
+        else if (a > b) Console.WriteLine($"{a.ToString()} > {b.ToString()}");
+
+        Console.WriteLine("\nПроверка на <= и >=");
+        if (a <= b) Console.WriteLine($"{a.ToString()} <= {b.ToString()}");
+        if (a >= b) Console.WriteLine($"{a.ToString()} >= {b.ToString()}");
+
+    }
+
+
+        
+
+}
